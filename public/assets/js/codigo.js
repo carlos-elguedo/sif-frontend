286089adeb9557c0c22124d5e5ef7454f6af986d
Carlos Elguedo <carlos-elguedo@hotmail.com>
2018-08-20 12:54:46 -0500

    All ready to start with the code

---
+++ public/assets/js/codigo.js
@@ -0,0 +1,39 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$(document).ready(function(){
+    //$('.sidenav').sidenav();
+    
+    $("#buscarTrabajador").click(function (){
+        //alert("BubuscarTrab");
+        $("#results_div").toggleClass("hide");
+        
+        $("#results").html('<img class="center-align responsive-img" src="../assets/images/progress.gif">');
+        
+        setTimeout(function () {
+            $("#results").html('');
+            
+            for (var i = 0, max = 4; i < max; i++) {
+                $("#results").append('<div class="col s12 m6 l3">'+
+                        '<div class="card">'+
+                        '<div class="card-image">'+
+                            '<img  class="img_result" src="../assets/images/perfil/'+ i +'.jpeg">'+
+                            '<span class="card-title">'+ i +'</span>'+
+                        '</div>'+
+                        '<div class="card-content">'+
+                            '<p>Nombre del trabajador '+ i +'</p>'+
+                        '</div>'+
+                        '<div class="card-action">'+
+                            '<a href="#">Ver perfil</a>'+
+                        '</div>'+
+                        '</div>'+
+                    '</div>');
+            }
+        }, 1000);
+    });
+    
+    
+  });
