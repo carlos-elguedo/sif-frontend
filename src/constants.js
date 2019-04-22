1b6bfbedc0430ceddcafd600b0bcd17a795877a9
Carlos Elguedo <carlos-elguedo@hotmail.com>
2019-04-21 23:31:47 -0500

    Store added - constants file

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+export const WORKER_ROUTES = {
+  root: '/worker',
+  edit: '/worker/edit'
+};
+
+export const REQUEST_STATUSES = {
+  NOT_LOADED: 'notLoaded',
+  LOADING: 'loading',
+  LOADED: 'loaded',
+  FAILED: 'failed'
+};
